e9e09560c9bd253cb10102795df95c53e9865275
luffi0804 <luffi0804@gmail.com>
2022-03-17 10:50:02 -0500

    Repository Validation BookName

---
+++ BiblioNetAPP/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using BiblioNetAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP.Controllers
+{
+    public class BookController: Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(book);
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ BiblioNetAPP/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage = "{0} is required")]
+
+        [Required(ErrorMessage = "Name is required")]
+
+        public string BookName { get; set; }
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
